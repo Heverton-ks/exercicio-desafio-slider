4e035d47f5b74c3a69eddb5e26b4d0f7fccc4749
Heverton ks <kelviheverton@gmail.com>
2024-09-11 13:26:08 -0300

    Acrescentado o README, alterado o nome de algumas classes, a informação de fonte foi mudada do arquivo style para o arquivo reset e no arquivo index.html ao invés de uma única caixa sendo editada para cada página, foi definido uma caixa para cada tela (embora no momento elas estejam sobrepostas).

---
+++ README.md
@@ -0,0 +1,42 @@
+## Bem-vindo(a) ao meu perfil
+
+ <div>
+   <img height="180em" src="https://github-readme-stats.vercel.app/api?username=heverton-ks&show_icons=true&theme=tokyonight&include_all_commits=true&count_private=true"/>
+   <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=heverton-ks&layout=compact&langs_count=6&theme=tokyonight"/>
+</div>
+    
+<div style="display: inline_block"><br>
+    <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+    <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+    <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+</div>
+ 
+<br>
+
+## Sobre
+
+Este é um exercício que realizei no meu curso de desenvolvimento front-end. 
+Apesar de ter construído o site de forma a atender os requisitos do enunciado, algumas melhorias preciam ser realizadas, assim sendo decidi subir o código para um repositório e ir atualizando.
+
+
+
+### Exercício proposto 
+
+Crie um carrossel (slider) de imagens
+
+  * O slider possui uma seta a esquerda e outra
+    a direita que servem pra passar ou voltar a
+    imagem.
+
+  * Quando for a primeira imagem, a seta da
+    esquerda deve ficar com uma opacidade e
+    sem clique.
+
+  * Quando for a última imagem, a seta da
+    direita deve ficar com uma opacidade e sem
+    clique.
+
+
+### Animação do site
+
+<img alt="animacao do site" src="src/gif.gif">
--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const btnSetaDireita = document.querySelector(".seta-direita");
 const btnSetaEsquerda = document.querySelector(".seta-esquerda");
     btnSetaEsquerda.classList.add("opacidade");
 
-let tituloBox = document.querySelector(".titulo");
-        tituloBox.innerHTML =`Titulo imagem 1`;
-
 const imagemFundo = document.querySelector("body");
-console.log(imagemFundo)
 imagemFundo.classList.add("fundo1");
 
     btnSetaDireita.addEventListener('click', () => {
@@ -18,18 +14,15 @@ imagemFundo.classList.add("fundo1");
             saibaMais.classList.remove("vermelho");
             saibaMais.classList.add("amarelo");
             btnSetaEsquerda.classList.remove("opacidade")
-            tituloBox.innerHTML =`Titulo imagem 2`;
             imagemFundo.classList.add("fundo2");
         } else if(saibaMais.classList.contains('amarelo')){
             saibaMais.classList.remove("amarelo");
             saibaMais.classList.add("marrom");
-            tituloBox.innerHTML =`Titulo imagem 3`;
             imagemFundo.classList.add("fundo3");
         } else if(saibaMais.classList.contains('marrom')){
             saibaMais.classList.remove("marrom");
             saibaMais.classList.add("azul");
             btnSetaDireita.classList.add("opacidade");
-            tituloBox.innerHTML =`Titulo imagem 4`;
             imagemFundo.classList.add("fundo4");
         }
     });
@@ -40,20 +33,17 @@ imagemFundo.classList.add("fundo1");
             saibaMais.classList.remove("azul");
             saibaMais.classList.add("marrom");
             btnSetaDireita.classList.remove("opacidade")
-            tituloBox.innerHTML =`Titulo imagem 3`;
             imagemFundo.classList.remove("fundo4");
             imagemFundo.classList.add("fundo3");
         } else if(saibaMais.classList.contains('marrom')){
             saibaMais.classList.remove("marrom");
             saibaMais.classList.add("amarelo");
-            tituloBox.innerHTML =`Titulo imagem 2`;
             imagemFundo.classList.remove("fundo3");
             imagemFundo.classList.add("fundo2");
         } else if(saibaMais.classList.contains('amarelo')){
             saibaMais.classList.remove("amarelo");
             saibaMais.classList.add("vermelho");
             btnSetaEsquerda.classList.add("opacidade");
-            tituloBox.innerHTML =`Titulo imagem 1`;
             imagemFundo.classList.remove("fundo2");
             imagemFundo.classList.add("fundo1");
         } 
