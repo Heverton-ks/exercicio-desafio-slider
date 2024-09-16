4b07a1deeba0d1cab4d9e88ec82716b3b72fda50
Heverton ks <kelviheverton@gmail.com>
2024-09-16 15:17:57 -0300

    No código index.js foi adicionado a variável 'mostrarCaixa' com a finalidade de que uma caixa possa sobrepor a outra conforme o click das setas. A classe 'mostrar', presente nos três arquivos alterados, também foi adicionado com a mesma finalidade.

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-const saibaMais = document.querySelector(".link");
-
 const btnSetaDireita = document.querySelector(".seta-direita");
 
 const btnSetaEsquerda = document.querySelector(".seta-esquerda");
-    btnSetaEsquerda.classList.add("opacidade");
+btnSetaEsquerda.classList.add("opacidade");
 
 const imagemFundo = document.querySelector("body");
 imagemFundo.classList.add("fundo1");
 
-    btnSetaDireita.addEventListener('click', () => {
-		
-        if(imagemFundo.classList.contains('fundo1')){
-            btnSetaEsquerda.classList.remove("opacidade")
-            imagemFundo.classList.remove("fundo1");
-            imagemFundo.classList.add("fundo2");
-        } else if(imagemFundo.classList.contains('fundo2')){ 
-            imagemFundo.classList.remove("fundo2");
-            imagemFundo.classList.add("fundo3");
-        } else if(imagemFundo.classList.contains('fundo3')){
+const mostrarCaixa = document.querySelectorAll(".caixa");
 
-            btnSetaDireita.classList.add("opacidade");
-            imagemFundo.classList.remove("fundo3");
-            imagemFundo.classList.add("fundo4");
-        }
-    });
+btnSetaDireita.addEventListener('click', () => {
 
-    btnSetaEsquerda.addEventListener('click', () => {
-            
-        if(imagemFundo.classList.contains('fundo4')){
-            btnSetaDireita.classList.remove("opacidade")
-            imagemFundo.classList.remove("fundo4");
-            imagemFundo.classList.add("fundo3");
-        } else if(imagemFundo.classList.contains('fundo3')){
+    if (imagemFundo.classList.contains('fundo1')) {
+        btnSetaEsquerda.classList.remove("opacidade")
+        imagemFundo.classList.remove("fundo1");
+        imagemFundo.classList.add("fundo2");
+        mostrarCaixa[0].classList.remove("mostrar");
+        mostrarCaixa[1].classList.add("mostrar");
+    } else if (imagemFundo.classList.contains('fundo2')) {
+        imagemFundo.classList.remove("fundo2");
+        imagemFundo.classList.add("fundo3");
+        mostrarCaixa[1].classList.remove("mostrar");
+        mostrarCaixa[2].classList.add("mostrar");
+    } else if (imagemFundo.classList.contains('fundo3')) {
+        btnSetaDireita.classList.add("opacidade");
+        imagemFundo.classList.remove("fundo3");
+        imagemFundo.classList.add("fundo4");
+        mostrarCaixa[2].classList.remove("mostrar");
+        mostrarCaixa[3].classList.add("mostrar");
+    }
+});
 
-            imagemFundo.classList.remove("fundo3");
-            imagemFundo.classList.add("fundo2");
-        } else if(imagemFundo.classList.contains('fundo2')){
-            btnSetaEsquerda.classList.add("opacidade");
-            imagemFundo.classList.remove("fundo2");
-            imagemFundo.classList.add("fundo1");
-        } 
-    });
+btnSetaEsquerda.addEventListener('click', () => {
 
-   
+    if (imagemFundo.classList.contains('fundo4')) {
+        btnSetaDireita.classList.remove("opacidade")
+        imagemFundo.classList.remove("fundo4");
+        imagemFundo.classList.add("fundo3");
+        mostrarCaixa[3].classList.remove("mostrar");
+        mostrarCaixa[2].classList.add("mostrar");
+    } else if (imagemFundo.classList.contains('fundo3')) {
+        imagemFundo.classList.remove("fundo3");
+        imagemFundo.classList.add("fundo2");
+        mostrarCaixa[2].classList.remove("mostrar");
+        mostrarCaixa[1].classList.add("mostrar");
+    } else if (imagemFundo.classList.contains('fundo2')) {
+        btnSetaEsquerda.classList.add("opacidade");
+        imagemFundo.classList.remove("fundo2");
+        imagemFundo.classList.add("fundo1");
+        mostrarCaixa[1].classList.remove("mostrar");
+        mostrarCaixa[0].classList.add("mostrar");
+    }
+});
+
+
